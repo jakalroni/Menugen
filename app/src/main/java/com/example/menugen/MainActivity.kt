8346477f104e4fef46c9bf13a986e875ca061633
BELUGOD <1202409@naver.com>
2022-08-14 14:30:39 +0900

    Push test

--- app/src/main/java/com/example/menugen/MainActivity.kt
+++ app/src/main/java/com/example/menugen/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.menugen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.menugen.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.skip.setOnClickListener {
+            val intent = Intent(this, Survey::class.java)
+            startActivity(intent)
+        }
     }
 }
