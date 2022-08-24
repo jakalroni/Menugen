a25e88583606cb2a2714d05288d7c63d6c77e957
BELUGOD <1202409@naver.com>
2022-08-24 15:42:41 +0900

    0824 사진 변경, 관리화면 추가

--- app/src/main/java/com/example/menugen/Recommend.kt
+++ app/src/main/java/com/example/menugen/Recommend.kt
@@ -11,15 +11,18 @@ class Recommend : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recommend)
 
+        /*
         val large_menu1 = findViewById<LinearLayout>(R.id.large_menu1)
         val large_menu2 = findViewById<LinearLayout>(R.id.large_menu2)
         val large_menu3 = findViewById<LinearLayout>(R.id.large_menu3)
         val large_menu4 = findViewById<LinearLayout>(R.id.large_menu4)
+        */
 
         val userall = intent.getStringExtra("alllist").toString()
         // val alltext = findViewById<TextView>(R.id.idEdit)
         // alltext.setText(userall)
 
+        /*
         large_menu1.setOnClickListener {
             val newintent = Intent(this, Recommend2::class.java)
             newintent.putExtra("large_menu", "1")
@@ -43,5 +46,6 @@ class Recommend : AppCompatActivity() {
             newintent.putExtra("large_menu", "4")
             startActivity(newintent)
         }
+        */
     }
 }
