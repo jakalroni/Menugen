37507a4d7d49ca1d416b3365ad76bdc8d1e65d16
BELUGOD <1202409@naver.com>
2022-08-30 15:24:27 +0900

    0830 리사이클러뷰 버튼을 해결하라

--- app/src/main/java/com/example/menugen/Management_1.kt
+++ app/src/main/java/com/example/menugen/Management_1.kt
@@ -1,39 +1,27 @@
 package com.example.menugen
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.example.menugen.databinding.FragmentManagement1Binding
 import com.example.menugen.databinding.FragmentSettingBinding
+import java.lang.reflect.Member
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Management_1.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Management_1 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     // Activity 값을 가져오기 위한 선언 <------------------------------------ 여기부터
     private var _binding: FragmentManagement1Binding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -49,6 +37,7 @@ class Management_1 : Fragment() {
         btn_change.setOnClickListener {
             recommend.changeFragment(2)
         }
+
         return view
     }
 
@@ -61,63 +50,43 @@ class Management_1 : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         var meal = AutoLogin.getUserMeal(requireContext())
 
+
         // <밥>을 클릭했을 경우
         binding.tag1.setOnClickListener{
-            // 소분류에 보여지면서 음식 제공
-            val list1 = binding.List1
-            list1.visibility = View.VISIBLE
-            val list2 = binding.List2
-            list2.visibility = View.VISIBLE
-            val list3 = binding.List3
-            list3.visibility = View.VISIBLE
+            // 유동적인 리스트 생성을 위한 RecyclerView 객체와 변수들
+            val items = mutableListOf<String>()
+            items.add("감자밥(231g)")
+            items.add("검정콩밥(200g)")
+            items.add("계란덮밥(367g)")
 
-            val list1a = binding.List1a
-            list1a.setText("감자밥")
-            val list2a = binding.List2a
-            list2a.setText("검정콩밥")
-            val list3a = binding.List3a
-            list3a.setText("계란덥밥")
+            val rv = view.findViewById<RecyclerView>(R.id.foodlist)
+            val rvAdapter = RVAdapter(items)
+
+            rv.adapter = rvAdapter
+            rv.layoutManager = LinearLayoutManager(context)
         }
+
 
         binding.tag2.setOnClickListener{
-            // 소분류에 보여지면서 음식 제공
-            val list1 = binding.List1
-            list1.visibility = View.VISIBLE
-            val list2 = binding.List2
-            list2.visibility = View.VISIBLE
-            val list3 = binding.List3
-            list3.visibility = View.VISIBLE
+            // 유동적인 리스트 생성을 위한 RecyclerView 객체와 변수들
+            val items = mutableListOf<String>()
+            items.add("가지냉국(387g)")
+            items.add("갈비탕(1136g)")
+            items.add("감자된장국(528g)")
 
-            val list1a = binding.List1a
-            list1a.setText("국수장국")
-            val list2a = binding.List2a
-            list2a.setText("군만두")
-            val list3a = binding.List3a
-            list3a.setText("김치만두")
+            val rv = view.findViewById<RecyclerView>(R.id.foodlist)
+
+            val rvAdapter = RVAdapter(items)
+
+            rv.adapter = rvAdapter
+            rv.layoutManager = LinearLayoutManager(context)
         }
-
+/*
         binding.List1a.setOnClickListener{
 
         }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Management_1.
          */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Management_1().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+
     }
 }
